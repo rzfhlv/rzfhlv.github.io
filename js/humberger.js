234926142f00ffec88e9352457e3dcedb1ac38db
rzfhlv <rzfhlv.me@gmail.com>
2023-10-10 21:00:01 +0700

    fix work history and responsive

---
+++ js/humberger.js
@@ -0,0 +1,19 @@
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const closeIcon = document.getElementById("close-icon");
+let isOpen = false;
+
+menuToggle.addEventListener("click", () => {
+    isOpen = !isOpen;
+    if (isOpen) {
+        // Change to close icon
+        hamburgerIcon.classList.add("hidden");
+        closeIcon.classList.remove("hidden");
+    } else {
+        // Change back to hamburger icon
+        hamburgerIcon.classList.remove("hidden");
+        closeIcon.classList.add("hidden");
+    }
+    menu.classList.toggle("hidden");
+});
