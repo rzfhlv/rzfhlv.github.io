6689c24c1eb0ed30c3ebe1725797d25eae9d99e3
rzfhlv <rzfhlv.me@gmail.com>
2023-10-10 14:44:59 +0700

    fix email

---
+++ js/smoothScroll.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function smoothScroll(target) {
+        const element = document.querySelector(target);
+        if (element) {
+            window.scrollTo({
+                top: element.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    }
+
+    // Add smooth scrolling to all links with a hash
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            smoothScroll(targetId);
+        });
+    });
+});
